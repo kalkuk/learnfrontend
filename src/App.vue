dca3d485787acf91bcff33b579228e9067c88f97
Merle Jõgisman <kalku@lap.ee>
2024-02-25 13:23:13 +0200

    add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>Hello Vue</h1>
+  <div class="container">
+    <button v-on:click="count++" class="button is-primary my-4">Click Me! Count: {{ count }}</button>
+  </div>
 </template>
+  <script setup>
+    import { ref } from 'vue';
+    let count = ref(0);
+  </script>
